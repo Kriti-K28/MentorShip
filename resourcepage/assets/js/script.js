059b14e817c1bea2a285e7ca9f5ce4da79a99851
Kriti Kumari <1905243@kiit.ac.in>
2021-02-17 23:39:07 +0530

    Added resource page

---
+++ resourcepage/assets/js/script.js
@@ -0,0 +1,249 @@
+/**
+* Template Name: Arsha - v3.0.3
+* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
+    "use strict";
+  
+    // Preloader
+    $(window).on('load', function() {
+      if ($('#preloader').length) {
+        $('#preloader').delay(100).fadeOut('slow', function() {
+          $(this).remove();
+        });
+      }
+    });
+  
+    // Smooth scroll for the navigation menu and links with .scrollto classes
+    var scrolltoOffset = $('#header').outerHeight() - 2;
+    $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        if (target.length) {
+          e.preventDefault();
+  
+          var scrollto = target.offset().top - scrolltoOffset;
+          if ($(this).attr("href") == '#header') {
+            scrollto = 0;
+          }
+  
+          $('html, body').animate({
+            scrollTop: scrollto
+          }, 1500, 'easeInOutExpo');
+  
+          if ($(this).parents('.nav-menu, .mobile-nav').length) {
+            $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+            $(this).closest('li').addClass('active');
+          }
+  
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').fadeOut();
+          }
+          return false;
+        }
+      }
+    });
+  
+    // Activate smooth scroll on page load with hash links
+    $(document).ready(function() {
+      if (window.location.hash) {
+        var initial_nav = window.location.hash;
+        if ($(initial_nav).length) {
+          var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+          $('html, body').animate({
+            scrollTop: scrollto
+          }, 1500, 'easeInOutExpo');
+        }
+      }
+    });
+  
+    // Mobile Navigation
+    if ($('.nav-menu').length) {
+      var $mobile_nav = $('.nav-menu').clone().prop({
+        class: 'mobile-nav d-lg-none'
+      });
+      $('body').append($mobile_nav);
+      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+      $('body').append('<div class="mobile-nav-overly"></div>');
+  
+      $(document).on('click', '.mobile-nav-toggle', function(e) {
+        $('body').toggleClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        $('.mobile-nav-overly').toggle();
+      });
+  
+      $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+        e.preventDefault();
+        $(this).next().slideToggle(300);
+        $(this).parent().toggleClass('active');
+      });
+  
+      $(document).click(function(e) {
+        var container = $(".mobile-nav, .mobile-nav-toggle");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            $('.mobile-nav-overly').fadeOut();
+          }
+        }
+      });
+    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+      $(".mobile-nav, .mobile-nav-toggle").hide();
+    }
+    
+    
+    // Navigation active state on scroll
+    $(document).ready(function(){
+      $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) { 
+          $(".navbar").css("background", "rgb(245, 245, 245)"); 
+          $(".navbar").css("transition", "0.5s ease");
+
+          if(getWidth()>990){
+            $(".main_nav").css("color","grey");
+          }else{
+            $(".toggle_icon").attr("src","images/blue.png");
+            $(".navbar-toggler").css("transition", "0.5s ease");
+            $(".main_nav").css("color","white");
+          }
+
+          document.getElementsByClassName("techtable-image")[0].setAttribute("src", "images/TT logo.png");
+          $(".techtable-image").css("transition", "0.5s ease");
+        } else {
+          $(".navbar").css("background", "transparent");
+          
+          if(getWidth()>990){
+            $(".main_nav").css("color","white");
+          }else{
+            $(".toggle_icon").attr("src","images/white.png");
+            $(".navbar-toggler").css("transition", "0.5s ease");
+            $(".main_nav").css("color","white");
+          }
+
+           document.getElementsByClassName("techtable-image")[0].setAttribute("src", "images/TT logo (1).png");
+           $(".techtable-image").css("transition", "0.5s ease");        
+        }
+      });
+    });
+
+    // Toggle .header-scrolled class to #header when page is scrolled
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
+      } else {
+        $('#header').removeClass('header-scrolled');
+      }
+    });
+  
+    if ($(window).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    }
+  
+    // Back to top button
+    // $(window).scroll(function() {
+    //   if ($(this).scrollTop() > 100) {
+    //     $('.back-to-top').fadeIn('slow');
+    //   } else {
+    //     $('.back-to-top').fadeOut('slow');
+    //   }
+    // });
+  
+    // $('.back-to-top').click(function() {
+    //   $('html, body').animate({
+    //     scrollTop: 0
+    //   }, 1500, 'easeInOutExpo');
+    //   return false;
+    // });
+  
+    // Skills section
+    $('.skills-content').waypoint(function() {
+      $('.progress .progress-bar').each(function() {
+        $(this).css("width", $(this).attr("aria-valuenow") + '%');
+      });
+    }, {
+      offset: '80%'
+    });
+    // Init AOS
+    function aos_init() {
+      AOS.init({
+        duration: 1000,
+        once: true
+      });
+    }
+    $(window).on('load', function() {
+      aos_init();
+    });
+  
+  })(jQuery);
+
+  function getWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+  }
+
+  function navButtonColor(id) {
+    var e = document.getElementsByClassName("nav_button"); 
+    for(let i=0;i<e.length;i++){
+      e.item(i).style.backgroundColor="white";
+      e.item(i).style.color="#2454c1";
+      e.item(i).style.transition="0.5s ease";
+    }
+    var a =document.getElementById(id);
+    a.style.backgroundColor="#2454c1";
+    a.style.color="white";
+    a.style.transition="0.5s ease";
+  }
+  navButtonColor('video-tab');
+  
+
+
+  /*//youtube_player
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      // height: '350',
+      // width: '400',
+      videoId: 'Ooa2peVxmII',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }*/
